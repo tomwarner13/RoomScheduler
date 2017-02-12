970e1a85e60c4e9afe8c9f41bbc8d2e40aa03c33
tom <tomwarner13@gmail.com>
2017-02-12 18:29:28 -0500

    all endpoints exist, some work

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectID;
 const assert = require('assert');
 
 const bodyParser = require('body-parser');
@@ -41,6 +42,27 @@ function clearRooms(db, callback) {
     callback();
 }
 
+function checkEvent(newEvent, events) {
+  if(newEvent.start >= newEvent.end) {
+    return false; //cannot have zero or negative-length event
+  }
+
+  for(var i in events) {
+    var event = events[i];
+
+    if(event.end > newEvent.start) {
+      if(event.start >= newEvent.end) {
+        events.splice(i, 0, newEvent);
+        return true;
+      }
+      return false;
+    }
+  }
+
+  events.push(newEvent);
+  return true;
+}
+
 
 app.get('/api/rooms', (req, res) =>
 {
@@ -53,6 +75,34 @@ app.get('/api/rooms', (req, res) =>
       assert.equal(err, null);
       res.json(docs);
     })
+  });
+})
+
+app.get('/api/rooms/:id', (req, res) =>
+{
+  MongoClient.connect(dbUrl, function(err, db) {
+    assert.equal(null, err);
+
+    var rooms = db.collection('rooms');
+
+    rooms.findOne({ _id: new ObjectId(req.params.id) }, function(err, doc) {
+        assert.equal(null, err);
+        res.json(doc);
+    });
+  });
+})
+
+app.get('/api/rooms/name/:name', (req, res) =>
+{
+  MongoClient.connect(dbUrl, function(err, db) {
+    assert.equal(null, err);
+
+    var rooms = db.collection('rooms');
+
+    rooms.findOne({ name: req.params.name }, function(err, doc) {
+        assert.equal(null, err);
+        res.json(doc);
+    });
   });
 })
 
@@ -71,6 +121,20 @@ app.post('/api/rooms/clear', (req, res) => {
     clearRooms(db, () => db.close());
   });
   res.send("cleared");
+})
+
+//stubbing out rest of API for now so there's something to develop against
+
+app.post('/api/rooms/:name/events', (req, res) => {
+    //add event
+})
+
+app.put('api/rooms/:name/events/:eventId', (req, res) => {
+    //modify event
+})
+
+app.delete('api/rooms/:name/events/:eventId', (req, res) => {
+    //delete event
 })
 
 app.listen(3000, function () {
